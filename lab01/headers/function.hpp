10c13730e94c1ad178038a3f785ff8d7624d8996
Duy Le <duyla25clc@gmail.com>
2024-01-18 16:52:38 +0700

    s

---
+++ lab01/headers/function.hpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+struct interval {
+    int l, r;
+};
+
+void input(int &n, int* &p, int* &t, interval* &ii);
+int process(int n, int* p, int* t, interval* ii);
+void output(int ans);
+void clear();
