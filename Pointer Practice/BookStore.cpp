9d042a1e1704939c8606239223da15df66d93f6b
Duy Le <duyla25clc@gmail.com>
2024-01-18 08:17:11 +0700

    Changes to be committed: 	modified:   BookStore.cpp 	added   :   3 functions to input, output the data and a function 	to clear the allocated memory after use

--- Pointer Practice/BookStore.cpp
+++ Pointer Practice/BookStore.cpp
@@ -14,11 +14,32 @@ struct Book {
 };
 
 void inputTitle(char* &title);
+void inputStore(Book* &bookStore, int &n);
+void ouputStore(Book* bookStore, int n);
+void clearHeap(Book* bookStore, int n);
 
 int main() { 
     int n;
-    cin >> n;
     Book *bookStore;
+    inputStore(bookStore, n);
+    ouputStore(bookStore, n);
+    clearHeap(bookStore, n);
+    return 0;
+}
+
+void clearHeap(Book* bookStore, int n) {
+    for (int i = 0; i < n; i ++)
+        delete[] bookStore[i].title;
+    delete[] bookStore;
+}
+
+void ouputStore(Book* bookStore, int n) {
+    for (int i = 0; i < n; i ++)
+        cout << bookStore[i].ID << ' ' << bookStore[i].title << ' ' << bookStore[i].price << '\n';
+}
+
+void inputStore(Book* &bookStore, int &n) {
+    cin >> n;
     bookStore = new Book[n]();
     for (int i = 0; i < n; i ++) {
         cin >> (bookStore + i)->ID; 
@@ -27,17 +48,7 @@ int main() {
         
         cin >> (bookStore + i)->price; 
     }
-
-    for (int i = 0; i < n; i ++) {
-        cout << bookStore[i].ID << ' ' << bookStore[i].title << ' ' << bookStore[i].price << '\n';
-    }
-
-    for (int i = 0; i < n; i ++)
-        delete[] bookStore[i].title;
-    delete[] bookStore;
-
-    return 0;
-}   
+}
 
 void inputTitle(char* &title) {
     int curSz = 0;
